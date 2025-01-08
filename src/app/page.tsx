7d9c831cf8c80ef89b294fefbdd95748e6c840b0
GoMunster <gomunster@gmail.com>
2025-01-07 18:36:26 -0600

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { MarketingDashboard } from '@/components/dashboard/marketing-dashboard'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <MarketingDashboard />
+    </main>
+  )
+}
